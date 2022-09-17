7d17ab8cf07073f76bbae1bb4cd3622ef5a32dd9
YanSouzaa <djyansouza@gmail.com>
2022-09-17 01:01:29 -0300

    Initial commit

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,18 @@
+
+function LoginScreen() {
+  return (
+    <div>
+      <h1>Clinica Phayan</h1>
+      <input type='text' />
+      <br />
+      <input type='password' />
+      <br />
+      <button onSubmit />
+
+    </div>
+
+
+  );
+}
+
+export default LoginScreen;
