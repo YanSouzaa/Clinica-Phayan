10419b88d0bf85a3d35dfb9f2a47d6eb3a765cdc
YanSouzaa <djyansouza@gmail.com>
2022-11-02 11:59:29 -0300

    Conserto de novo agendamento

--- src/screens/SchedulesScreen.js
+++ src/screens/SchedulesScreen.js
@@ -21,16 +21,18 @@ const customStyles = {
 
 function SchedulesScreen() {
 
- 
+
   const [modalIsOpen, setIsOpen] = useState(false);
   const [doctors, setDoctors] = useState([]);
   const [schedules, setSchedules] = useState([]);
+  const [values, setValues] = useState();
+  const [selectValue, setSelectValue] = useState([])
 
   useEffect(() => {
     const retrieveSchedules = async () => {
       const retrievedSchedules = await transactionalDbService.getSchedules()
       setSchedules(retrievedSchedules.data.Items)
-    
+
     }
     retrieveSchedules()
   }, [])
@@ -43,8 +45,20 @@ function SchedulesScreen() {
     setDoctors(retrievedDoctors.data.Items);
     setIsOpen(true);
   }
+  const handleChangeValues = value => {
+    setValues(prevValue => ({
+      ...prevValue,
+      [value.target.name]: value.target.value,
+    }))
+  }
 
-  
+  const handleChangeValuesSelect = (event) => {
+    setSelectValue(event.value)
+  }
+  const selectedOptionDoctor = doctors.find(e => e.value === selectValue
+    )
+
+
   function closeModal() {
     setIsOpen(false);
   }
@@ -54,10 +68,10 @@ function SchedulesScreen() {
     event.preventDefault();
 
     const schedule = {
-     doctor_id: event.target.elements.doutor.value,
-     patient_id: event.target.elements.paciente.value,
-     date: event.target.elements.data.value,
-     hour: event.target.elements.horario.value,
+      doctor_id: selectValue,
+      patient_id: values.paciente,
+      date: values.data,
+      hour: values.horario,
     }
     await transactionalDbService.addSchedule(schedule)
     document.location.reload()
@@ -73,7 +87,7 @@ function SchedulesScreen() {
         </a>
       </nav>
       <div style={{ textAlign: 'center' }}>
-      <Modal
+        <Modal
           isOpen={modalIsOpen}
           onRequestClose={closeModal}
           style={customStyles}
@@ -83,20 +97,23 @@ function SchedulesScreen() {
 
           <form>
             <h4>Paciente</h4>
-            <input className='form-control' name="paciente"></input><br />
+            <input className='form-control' onChange={handleChangeValues} type='text' name="paciente"></input><br />
             <h4>Doutor(a)</h4>
             <Select
+              name='doutor'
+              placeholder='Selecione'
               options={doctors}
-
+              onChange={handleChangeValuesSelect}
+              value={selectedOptionDoctor}
             /><br />
             <h4>Data</h4>
-            <input type={'date'} className="form-control" name="data"></input><br />
+            <input type={'date'} onChange={handleChangeValues} className="form-control" name="data"></input><br />
             <h4>Horario</h4>
-            <input type={'time'} className="form-control" name="horario"></input><br />
+            <input type={'time'} onChange={handleChangeValues} className="form-control" name="horario"></input><br />
 
             <button onClick={handleSubmit} className="btn btn-primary">Agendar</button>
           </form>
-          </Modal>
+        </Modal>
         <br />
         <div style={{ justifyContent: 'space-between', display: 'flex', flexDirection: 'row', height: 50, alignItems: 'center' }}>
           <h1></h1>
@@ -105,20 +122,20 @@ function SchedulesScreen() {
           <button onClick={openModal} style={{ marginRight: '2%' }} className="btn btn-primary">Novo agendamento</button>
         </div>
         <br />
-      
+
         <div style={{ justifyContent: 'center', display: 'flex' }}>
-          <table className="table table-striped" style={{ justifyContent: 'center',width: '80%', border: 'solid 3px #000', borderRadius:10,  }}>
-          <thead>
-            <tr style={{  backgroundColor: '#B0C4DE' }}>
-              <th>Paciente</th>
-              <th>Doutor</th>
-              <th>Data</th>
-              <th>Hora</th>
-            </tr>
-          </thead>
-          <tbody>
-            {schedules.map((schedule) => <Schedule key={schedule.id} id={schedule.id} paciente={schedule.patient_id} medico={schedule.doctor_id} data={schedule.date} horario={schedule.hour} />)}
-          </tbody>
+          <table className="table table-striped" style={{ justifyContent: 'center', width: '80%', border: 'solid 3px #000', borderRadius: 10, }}>
+            <thead>
+              <tr style={{ backgroundColor: '#B0C4DE' }}>
+                <th>Paciente</th>
+                <th>Doutor</th>
+                <th>Data</th>
+                <th>Hora</th>
+              </tr>
+            </thead>
+            <tbody>
+              {schedules.map((schedule) => <Schedule key={schedule.id} id={schedule.id} paciente={schedule.patient_id} medico={schedule.doctor_id} data={schedule.date} horario={schedule.hour} />)}
+            </tbody>
 
           </table>
         </div>
